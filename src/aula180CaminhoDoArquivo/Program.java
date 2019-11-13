aa64b5bed499a46fdd40bd32f46b27a464ca4785
Lucas Assis <lga.lima@hotmail.com>
2019-11-13 09:24:11 -0300

    Aula 180: Informações do caminho do arquivo

--- src/aula180CaminhoDoArquivo/Program.java
+++ src/aula180CaminhoDoArquivo/Program.java
@@ -1,7 +1,9 @@
+package aula180CaminhoDoArquivo;
+
 import java.io.File;
 import java.util.Scanner;
 
-public class aula180CaminhoDoArquivo {
+public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
