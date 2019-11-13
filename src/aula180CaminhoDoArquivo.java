63d873df400ed9ba94bb94f2126430978957ee0d
Lucas Assis <lga.lima@hotmail.com>
2019-11-13 09:16:17 -0300

    Aula 180: Informações do caminho do arquivo

---
+++ src/aula180CaminhoDoArquivo.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class aula180CaminhoDoArquivo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com o caminho do arquivo: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getPatent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
+	
+		sc.close();
+	}
+}
