b5a2de413dbed060390ac5d854fb4916d1322475
Lucas Assis <lga.lima@hotmail.com>
2019-11-14 17:16:23 -0300

    Aula 190: Herança vs Cumprir contrato interface

--- src/aula190HerdarVSCumprirContrato/Program.java
+++ src/aula190HerdarVSCumprirContrato/Program.java
@@ -3,7 +3,6 @@ package aula190HerdarVSCumprirContrato;
 import aula190HerdarVSCumprirContrato.model.entities.AbstractShape;
 import aula190HerdarVSCumprirContrato.model.entities.Circulo;
 import aula190HerdarVSCumprirContrato.model.entities.Retangulo;
-import aula190HerdarVSCumprirContrato.model.entities.Shape;
 import aula190HerdarVSCumprirContrato.model.enums.Cor;
 
 public class Program {
@@ -13,7 +12,7 @@ public class Program {
 		AbstractShape s2 = new Retangulo(Cor.BRANCO, 3.0, 4.0);
 		System.out.println("Cor do circulo: " + s1.getColor());
 		System.out.println("Area do circulo: " + String.format("%.3f", s1.area()));
-		System.out.println("Cor do retangulo: " + s1.getColor());
-		System.out.println("Area do retangulo: " + String.format("%.3f", s1.area()));
+		System.out.println("Cor do retangulo: " + s2.getColor());
+		System.out.println("Area do retangulo: " + String.format("%.3f", s2.area()));
 	}
 }
