1ac0faf4d77d5bdd57cc0bfd22b579cc4c5b6287
Lucas Assis <lga.lima@hotmail.com>
2020-07-09 18:29:28 -0300

    Aula 222: Pipeline

---
+++ src/aula222Pipeline/Program.java
@@ -0,0 +1,27 @@
+package aula222Pipeline;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = Arrays.asList(3, 4, 5, 10 ,7);
+		
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+	
+		int sum = list.stream().reduce(0,  (x, y) -> x + y);
+		System.out.println(sum);
+		
+		List<Integer> newList = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
+	}
+
+}
